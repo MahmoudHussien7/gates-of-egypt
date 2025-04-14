5bc23e692d477c752dc8475e2ebb47b326fa3ca9
Mahmoud Hussien Abdulhaliem <mahmoudhu37@gmail.com>
2025-04-14 16:09:30 +0200

    feat: :sparkles: enable analytics at vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Providers } from "./providers";
-
+import { Analytics } from "@vercel/analytics/react";
 export default function RootLayout({
   children,
 }: {
@@ -8,7 +8,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body cz-shortcut-listen="true">
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <Analytics />
+        </Providers>
       </body>
     </html>
   );
